7faa0de04b1cc00be24415aff14d656acedb6466
Matthias Walter <matthias.walter@mwltr.de>
2018-05-14 14:20:53 +0200

    add module skeleton and basic directory layout

---
+++ Test/Unit/DummyUnitTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright see LICENSE.txt
+ *
+ * @see LICENSE.txt
+ */
+
+namespace Eurotext\TranslationManager\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * DummyUnitTest
+ */
+class DummyUnitTest extends TestCase
+{
+    public function testDummyDoesNothing()
+    {
+        $this->assertTrue(true);
+    }
+}
