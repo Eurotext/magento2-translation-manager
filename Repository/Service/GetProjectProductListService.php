cf40407adffb81ea465aaed7d6dd06efd483fa74
Matthias Walter <matthias.walter@mwltr.de>
2018-06-08 12:36:05 +0200

    refactor GetListServices with less code duplication but still a clean implementation

--- Repository/Service/GetProjectProductListService.php
+++ Repository/Service/GetProjectProductListService.php
@@ -7,71 +7,27 @@
 
 namespace Eurotext\TranslationManager\Repository\Service;
 
-use Eurotext\TranslationManager\Model\ResourceModel\ProjectProductCollection;
 use Eurotext\TranslationManager\Model\ResourceModel\ProjectProductCollectionFactory;
-use Magento\Framework\Api\SearchCriteriaInterface;
-use Magento\Framework\Api\SearchResultsInterface;
 use Magento\Framework\Api\SearchResultsInterfaceFactory;
-use Magento\Framework\Api\SortOrder;
 
-class GetProjectProductListService
+class GetProjectProductListService extends AbstractGetListService
 {
     /**
      * @var ProjectProductCollectionFactory
      */
     private $collectionFactory;
 
-    /**
-     * @var SearchResultsInterfaceFactory
-     */
-    private $searchResultsFactory;
-
     public function __construct(
         ProjectProductCollectionFactory $collectionFactory,
         SearchResultsInterfaceFactory $searchResultsFactory
     ) {
+        parent::__construct($searchResultsFactory);
+
         $this->collectionFactory = $collectionFactory;
-        $this->searchResultsFactory = $searchResultsFactory;
     }
 
-    public function execute(SearchCriteriaInterface $criteria): SearchResultsInterface
+    protected function createCollection(): \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
     {
-        /** @var ProjectProductCollection $collection */
-        $collection = $this->collectionFactory->create();
-        foreach ($criteria->getFilterGroups() as $filterGroup) {
-            $fields = [];
-            $conditions = [];
-            foreach ($filterGroup->getFilters() as $filter) {
-                $condition = $filter->getConditionType() ?: 'eq';
-                $fields[] = $filter->getField();
-                $conditions[] = [$condition => $filter->getValue()];
-            }
-            if ($fields) {
-                $collection->addFieldToFilter($fields, $conditions);
-            }
-        }
-        $sortOrders = $criteria->getSortOrders();
-        if ($sortOrders) {
-            /** @var SortOrder $sortOrder */
-            foreach ($sortOrders as $sortOrder) {
-                $direction = ($sortOrder->getDirection() === SortOrder::SORT_ASC) ? 'ASC' : 'DESC';
-                $collection->addOrder($sortOrder->getField(), $direction);
-            }
-        }
-        $collection->setCurPage($criteria->getCurrentPage());
-        $collection->setPageSize($criteria->getPageSize());
-
-        $objects = [];
-        foreach ($collection as $objectModel) {
-            $objects[] = $objectModel;
-        }
-
-        /** @var \Magento\Framework\Api\SearchResultsInterface $searchResults */
-        $searchResults = $this->searchResultsFactory->create();
-        $searchResults->setSearchCriteria($criteria);
-        $searchResults->setTotalCount($collection->getSize());
-        $searchResults->setItems($objects);
-
-        return $searchResults;
+        return $this->collectionFactory->create();
     }
 }
