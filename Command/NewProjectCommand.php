13870e3e01be09015973702e1cd5ea92ce3a9b41
Matthias Walter <matthias.walter@mwltr.de>
2018-07-02 21:59:02 +0200

    Seems like Magento does not support Proxy definitions in di.xml?

--- Command/NewProjectCommand.php
+++ Command/NewProjectCommand.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Eurotext\TranslationManager\Command;
 
-use Eurotext\TranslationManager\Command\Service\NewProjectService;
+use Eurotext\TranslationManager\Command\Service\NewProjectService\Proxy as NewProjectService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
