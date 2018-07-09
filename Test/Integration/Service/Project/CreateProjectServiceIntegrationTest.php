089772721236fee1fb81c2fd426d7307a38abaec
Matthias Walter <matthias.walter@mwltr.de>
2018-07-09 15:46:22 +0200

    fix IntegratioTest CreateProjectService

--- Test/Integration/Service/Project/CreateProjectServiceIntegrationTest.php
+++ Test/Integration/Service/Project/CreateProjectServiceIntegrationTest.php
@@ -8,11 +8,14 @@ declare(strict_types=1);
 
 namespace Eurotext\TranslationManager\Test\Integration\Service\Project;
 
+use Eurotext\RestApiClient\Api\ProjectV1Api;
+use Eurotext\TranslationManager\Api\ProjectRepositoryInterface;
 use Eurotext\TranslationManager\ApiClient\Configuration;
+use Eurotext\TranslationManager\Mapper\ProjectPostMapper;
 use Eurotext\TranslationManager\Model\Project;
 use Eurotext\TranslationManager\Service\Project\CreateProjectService;
 use Eurotext\TranslationManager\Test\Integration\IntegrationTestAbstract;
-use Magento\Config\Model\ResourceModel\Config as ConfigResource;
+use Psr\Log\LoggerInterface;
 
 class CreateProjectServiceIntegrationTest extends IntegrationTestAbstract
 {
@@ -23,13 +26,29 @@ class CreateProjectServiceIntegrationTest extends IntegrationTestAbstract
     {
         parent::setUp();
 
-        /** @var ConfigResource $config */
-        $config = $this->objectManager->get(ConfigResource::class);
-        $config->saveConfig(Configuration::CONFIG_PATH_API_KEY, \constant('EUROTEXT_API_KEY'), 'default', 0);
-        $config->saveConfig(Configuration::CONFIG_PATH_API_HOST, 'https://sandbox.api.eurotext.de', 'default', 0);
-        $config->saveConfig(Configuration::CONFIG_PATH_API_DEBUG_MODE, '0', 'default', 0);
+//        /** @var ConfigResource $config */
+//        $config = $this->objectManager->get(ConfigResource::class);
+//        $config->saveConfig(Configuration::CONFIG_PATH_API_KEY, \constant('EUROTEXT_API_KEY'), 'default', 0);
+//        $config->saveConfig(Configuration::CONFIG_PATH_API_HOST, 'https://sandbox.api.eurotext.de', 'default', 0);
+//        $config->saveConfig(Configuration::CONFIG_PATH_API_DEBUG_MODE, '0', 'default', 0);
 
-        $this->sut = $this->objectManager->get(CreateProjectService::class);
+        $config = $this->getMockBuilder(Configuration::class)
+                       ->disableOriginalConstructor()
+                       ->setMethods(['getApiKey','getHost', 'getDebug'])
+                       ->getMock();
+        $config->expects($this->once())->method('getApiKey')->willReturn(\constant('EUROTEXT_API_KEY'));
+        $config->expects($this->once())->method('getHost')->willReturn('https://sandbox.api.eurotext.de');
+        $config->expects($this->once())->method('getDebug')->willReturn(false);
+
+        $projectApi = new ProjectV1Api($config);
+
+        $this->sut = new CreateProjectService(
+            $this->objectManager->get(ProjectRepositoryInterface::class),
+            $this->objectManager->get(ProjectPostMapper::class),
+            $projectApi,
+            $this->objectManager->get(LoggerInterface::class)
+        );
+        // $this->sut = $this->objectManager->get(CreateProjectService::class);
     }
 
     /**
