1272e06b55319e8f1c1ad81624a8a97a7feea39f
Matthias Walter <matthias.walter@mwltr.de>
2018-09-07 15:29:20 +0200

    ApiClient ProjectV1Api and ItemV1Api implementations in module to inject config

--- ApiClient/ItemV1Api.php
+++ ApiClient/ItemV1Api.php
@@ -9,16 +9,15 @@ declare(strict_types=1);
 namespace Eurotext\TranslationManager\ApiClient;
 
 use GuzzleHttp\ClientInterface;
-use Symfony\Component\Serializer\SerializerInterface;
 
-class ProjectV1Api extends \Eurotext\RestApiClient\Api\ProjectV1Api
+class ItemV1Api extends \Eurotext\RestApiClient\Api\Project\ItemV1Api
 {
     public function __construct(
         ConfigurationFactory $configFactory,
         ClientInterface $client = null
     ) {
         $config = $configFactory->create();
-
+        
         parent::__construct($config, $client);
     }
 }
