7d596f6b0df77850260bb0e0b17ae6c7227637c9
Matthias Walter <matthias.walter@mwltr.de>
2018-09-17 15:54:30 +0200

    refactor ProjectDataRequest => ProjectPostRequest

--- Mapper/ProjectPostMapper.php
+++ Mapper/ProjectPostMapper.php
@@ -10,16 +10,16 @@ namespace Eurotext\TranslationManager\Mapper;
 
 use Eurotext\RestApiClient\Enum\ProjectTypeEnum;
 use Eurotext\RestApiClient\Request\Data\ProjectData;
-use Eurotext\RestApiClient\Request\ProjectDataRequest;
+use Eurotext\RestApiClient\Request\ProjectPostRequest;
 use Eurotext\TranslationManager\Api\Data\ProjectInterface;
 
 class ProjectPostMapper
 {
-    public function map(ProjectInterface $project): ProjectDataRequest
+    public function map(ProjectInterface $project): ProjectPostRequest
     {
         $data = new ProjectData($project->getCustomerComment());
 
-        $request = new ProjectDataRequest($project->getName(), $data, ProjectTypeEnum::QUOTE());
+        $request = new ProjectPostRequest($project->getName(), $data, ProjectTypeEnum::QUOTE());
 
         return $request;
     }
