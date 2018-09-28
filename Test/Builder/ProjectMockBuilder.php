5f9fc3129e7d88889a0b36e17c075673cf47252f
Matthias Walter <matthias.walter@mwltr.de>
2018-09-28 13:35:49 +0200

    ProjectMockBuilder

--- Test/Builder/ProjectMockBuilder.php
+++ Test/Builder/ProjectMockBuilder.php
@@ -8,8 +8,10 @@ declare(strict_types=1);
 
 namespace Eurotext\TranslationManager\Test\Builder;
 
-use Eurotext\TranslationManager\Api\ProjectRepositoryInterface;
+use Eurotext\TranslationManager\Api\Data\ProjectInterface;
 use Eurotext\TranslationManager\Api\EntitySeederInterface;
+use Eurotext\TranslationManager\Api\ProjectRepositoryInterface;
+use Eurotext\TranslationManager\Model\Project;
 use Eurotext\TranslationManager\Model\ProjectFactory;
 use PHPUnit\Framework\TestCase;
 
@@ -26,35 +28,46 @@ class ProjectMockBuilder
     }
 
     /**
-     * @return \PHPUnit_Framework_MockObject_MockObject
+     * @return ProjectInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public function buildProjectMock(): \PHPUnit_Framework_MockObject_MockObject
+    {
+        return $this->getMockBuilder(Project::class)
+                    ->disableOriginalConstructor()
+                    // ->setMethods(['getId', ])
+                    ->getMock();
+    }
+
+    /**
+     * @return EntitySeederInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     public function buildProjectSeederMock(): \PHPUnit_Framework_MockObject_MockObject
     {
         return $this->getMockBuilder(EntitySeederInterface::class)
-            ->setMethods(['seed'])
-            ->getMockForAbstractClass();
+                    ->setMethods(['seed'])
+                    ->getMockForAbstractClass();
     }
 
     /**
-     * @return \PHPUnit_Framework_MockObject_MockObject
+     * @return ProjectRepositoryInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     public function buildProjectRepositoryMock(): \PHPUnit_Framework_MockObject_MockObject
     {
         return $this->getMockBuilder(ProjectRepositoryInterface::class)
-            ->setMethods(['save'])
-            ->disableOriginalConstructor()
-            ->getMockForAbstractClass();
+                    ->setMethods(['save'])
+                    ->disableOriginalConstructor()
+                    ->getMockForAbstractClass();
     }
 
     /**
-     * @return \PHPUnit_Framework_MockObject_MockObject
+     * @return ProjectFactory|\PHPUnit_Framework_MockObject_MockObject
      */
     public function buildProjectFactoryMock(): \PHPUnit_Framework_MockObject_MockObject
     {
         return $this->getMockBuilder(ProjectFactory::class)
-            ->setMethods(['create'])
-            ->disableOriginalConstructor()
-            ->getMock();
+                    ->setMethods(['create'])
+                    ->disableOriginalConstructor()
+                    ->getMock();
     }
 
     protected function getMockBuilder($className): \PHPUnit_Framework_MockObject_MockBuilder
