37271518bf2fef04775aa6a2e94eb507a376c35d
Matthias Walter <matthias.walter@mwltr.de>
2018-10-08 15:53:28 +0200

    ETM2-25 use ProjectStatusValidator before Items retrieval in ReceiveProjectCommand

--- Command/ReceiveProjectCommand.php
+++ Command/ReceiveProjectCommand.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace Eurotext\TranslationManager\Command;
 
+use Eurotext\RestApiClient\Enum\ProjectStatusEnum;
+use Eurotext\RestApiClient\Validator\ProjectStatusValidatorInterface;
 use Eurotext\TranslationManager\Api\Data\ProjectInterface;
+use Eurotext\TranslationManager\Api\ProjectRepositoryInterface;
 use Eurotext\TranslationManager\Logger\PushConsoleLogHandler;
 use Eurotext\TranslationManager\Service\ReceiveProjectService;
 use Eurotext\TranslationManager\State\ProjectStateMachine;
@@ -40,18 +43,32 @@ class ReceiveProjectCommand extends Command
      */
     private $projectStateMachine;
 
+    /**
+     * @var ProjectStatusValidatorInterface
+     */
+    private $projectStatusValidator;
+
+    /**
+     * @var ProjectRepositoryInterface
+     */
+    private $projectRepository;
+
     public function __construct(
         ReceiveProjectService $receiveProject,
+        ProjectRepositoryInterface $projectRepository,
+        ProjectStatusValidatorInterface $projectStatusValidator,
         ProjectStateMachine $projectStateMachine,
         PushConsoleLogHandler $pushConsoleLog,
         AppState $appState
     ) {
         parent::__construct();
 
-        $this->receiveProject = $receiveProject;
-        $this->pushConsoleLog = $pushConsoleLog;
-        $this->appState       = $appState;
-        $this->projectStateMachine = $projectStateMachine;
+        $this->receiveProject         = $receiveProject;
+        $this->projectRepository      = $projectRepository;
+        $this->projectStatusValidator = $projectStatusValidator;
+        $this->projectStateMachine    = $projectStateMachine;
+        $this->pushConsoleLog         = $pushConsoleLog;
+        $this->appState               = $appState;
     }
 
     protected function configure()
@@ -74,10 +91,19 @@ class ReceiveProjectCommand extends Command
             // the area code is already set
         }
 
+        // Load Project
+        $project = $this->projectRepository->getById($projectId);
+
         // Push the ConsoleLogger to the EurotextLogger so we directly see console output
         $this->pushConsoleLog->push($output);
 
-        // @todo Service: check API Project Status === finished
+        // check API Project Status === finished
+        $isFinished = $this->projectStatusValidator->validate($project, ProjectStatusEnum::FINISHED());
+        if (!$isFinished) {
+            $output->writeln('Eurotext Translation Project is not marked as finished');
+
+            return;
+        }
 
         // Set status ACCEPTED, because services are checking for the correct workflow
         $this->projectStateMachine->applyById($projectId, ProjectInterface::STATUS_TRANSLATED);
