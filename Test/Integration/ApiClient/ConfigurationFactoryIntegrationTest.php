0d07a5cc7efc6cda0c4c5ae4d5e20d7101e31e36
Matthias Walter <matthias.walter@mwltr.de>
2018-10-08 12:47:21 +0200

    disable source of the problem

--- Test/Integration/ApiClient/ConfigurationFactoryIntegrationTest.php
+++ Test/Integration/ApiClient/ConfigurationFactoryIntegrationTest.php
@@ -11,6 +11,7 @@ namespace Eurotext\TranslationManager\Test\Integration\ApiClient;
 use Eurotext\TranslationManager\ApiClient\ConfigurationFactory;
 use Eurotext\TranslationManager\Test\Integration\IntegrationTestAbstract;
 use Magento\Framework\App\Config\Storage\Writer as StorageWriter;
+use Magento\Framework\App\ProductMetadataInterface;
 
 class ConfigurationFactoryIntegrationTest extends IntegrationTestAbstract
 {
@@ -20,19 +21,26 @@ class ConfigurationFactoryIntegrationTest extends IntegrationTestAbstract
     /** @var StorageWriter */
     private $configWriter;
 
+    /** @var ProductMetadataInterface|\PHPUnit_Framework_MockObject_MockObject */
+    private $metadata;
+
     protected function setUp()
     {
         parent::setUp();
 
         $this->configWriter = $this->objectManager->get(StorageWriter::class);
 
-        $this->sut = $this->objectManager->get(ConfigurationFactory::class);
+        $this->metadata = $this->getMockBuilder(ProductMetadataInterface::class)->getMock();
+
+        $this->sut = $this->objectManager->create(ConfigurationFactory::class, ['magentoMetadata'=> $this->metadata]);
     }
 
     public function testConfiguration()
     {
         $apiKey = '1234567890';
         $this->configWriter->save(ConfigurationFactory::CONFIG_PATH_API_KEY, $apiKey);
+
+        $this->metadata->expects($this->once())->method('getVersion')->willReturn('2.x.x');
 
         $configuration = $this->sut->create();
 
