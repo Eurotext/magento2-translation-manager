178a5465b7599053628cdb4659969c2558e556c3
Matthias Walter <matthias.walter@mwltr.de>
2019-01-18 12:35:58 +0100

    ETM-35 refactor AddEntityCommand business logic to AddEntityService

--- Console/Command/AddEntityCommand.php
+++ Console/Command/AddEntityCommand.php
@@ -3,8 +3,7 @@ declare(strict_types=1);
 
 namespace Eurotext\TranslationManager\Console\Command;
 
-use Eurotext\TranslationManager\Api\ProjectRepositoryInterface;
-use Eurotext\TranslationManager\Seeder\EntitySeederPool;
+use Eurotext\TranslationManager\Console\Service\AddEntityService;
 use Magento\Framework\App\State as AppState;
 use Magento\Framework\Exception\LocalizedException;
 use Symfony\Component\Console\Command\Command;
@@ -17,38 +16,23 @@ class AddEntityCommand extends Command
     const NAME        = 'etm:entity:add';
     const DESCRIPTION = 'Adds one or more entites to an existing project';
 
-    const ARG_PROJECT_ID  = 'project-id';
-    const ARG_ENTITY_TYPE = 'entity-type';
-    const ARG_ENTITY_ID   = 'entity-id';
-
-    const ARG_PROJECT_ID_DESC  = 'the project id';
-    const ARG_ENTITY_TYPE_DESC = 'the entity-type you want to import, type overview with command etm:entity:types';
-    const ARG_ENTITY_ID_DESC   = 'Comma seperated list of entities to add. Depending on the entity-type the data provided may vary.';
-
     /**
      * @var AppState
      */
     private $appState;
 
     /**
-     * @var ProjectRepositoryInterface
+     * @var AddEntityService
      */
-    private $projectRepository;
-
-    /**
-     * @var EntitySeederPool
-     */
-    private $entitySeederPool;
+    private $addEntityService;
 
     public function __construct(
-        ProjectRepositoryInterface $projectRepository,
-        EntitySeederPool $entitySeederPool,
+        AddEntityService $addEntityService,
         AppState $appState
     ) {
         parent::__construct();
-        $this->appState          = $appState;
-        $this->projectRepository = $projectRepository;
-        $this->entitySeederPool  = $entitySeederPool;
+        $this->appState         = $appState;
+        $this->addEntityService = $addEntityService;
     }
 
     protected function configure()
@@ -56,9 +40,15 @@ class AddEntityCommand extends Command
         $this->setName(self::NAME);
         $this->setDescription(self::DESCRIPTION);
 
-        $this->addArgument(self::ARG_PROJECT_ID, InputArgument::REQUIRED, self::ARG_PROJECT_ID_DESC);
-        $this->addArgument(self::ARG_ENTITY_TYPE, InputArgument::REQUIRED, self::ARG_ENTITY_TYPE_DESC);
-        $this->addArgument(self::ARG_ENTITY_ID, InputArgument::REQUIRED, self::ARG_ENTITY_ID_DESC);
+        $this->addArgument(
+            AddEntityService::ARG_PROJECT_ID, InputArgument::REQUIRED, AddEntityService::ARG_PROJECT_ID_DESC
+        );
+        $this->addArgument(
+            AddEntityService::ARG_ENTITY_TYPE, InputArgument::REQUIRED, AddEntityService::ARG_ENTITY_TYPE_DESC
+        );
+        $this->addArgument(
+            AddEntityService::ARG_ENTITY_ID, InputArgument::REQUIRED, AddEntityService::ARG_ENTITY_ID_DESC
+        );
 
         parent::configure();
     }
@@ -71,29 +61,6 @@ class AddEntityCommand extends Command
             // the area code is already set
         }
 
-        $projectId  = (int)$input->getArgument(self::ARG_PROJECT_ID);
-        $entityCode = (string)$input->getArgument(self::ARG_ENTITY_TYPE);
-        $entityId   = (string)$input->getArgument(self::ARG_ENTITY_ID);
-
-        $entities = explode(',', $entityId);
-
-        $project = $this->projectRepository->getById($projectId);
-
-        try {
-            $entitySeeder = $this->entitySeederPool->getByCode($entityCode);
-        } catch (\Exception $e) {
-            $output->writeln(sprintf('<error>%s: seeder not found</error>', $entityCode));
-
-            return;
-        }
-
-        $result = $entitySeeder->seed($project, $entities);
-
-        if ($result === true) {
-            $output->writeln(sprintf('<info>%s: seeding successful</info>', $entityCode));
-        } else {
-            $output->writeln(sprintf('<error>%s: seeding not successful</error>', $entityCode));
-        }
-
+        $this->addEntityService->execute($input, $output);
     }
 } 
---
+++ Console/Service/AddEntityService.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+namespace Eurotext\TranslationManager\Console\Service;
+
+use Eurotext\TranslationManager\Api\ProjectRepositoryInterface;
+use Eurotext\TranslationManager\Seeder\EntitySeederPool;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AddEntityService
+{
+    const ARG_PROJECT_ID  = 'project-id';
+    const ARG_ENTITY_TYPE = 'entity-type';
+    const ARG_ENTITY_ID   = 'entity-id';
+
+    const ARG_PROJECT_ID_DESC  = 'the project id';
+    const ARG_ENTITY_TYPE_DESC = 'the entity-type you want to import, type overview with command etm:entity:types';
+    const ARG_ENTITY_ID_DESC   = 'Comma seperated list of entities to add. Depending on the entity-type the data provided may vary.';
+
+    /**
+     * @var ProjectRepositoryInterface
+     */
+    private $projectRepository;
+
+    /**
+     * @var EntitySeederPool
+     */
+    private $entitySeederPool;
+
+    public function __construct(ProjectRepositoryInterface $projectRepository, EntitySeederPool $entitySeederPool)
+    {
+        $this->projectRepository = $projectRepository;
+        $this->entitySeederPool  = $entitySeederPool;
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $projectId  = (int)$input->getArgument(self::ARG_PROJECT_ID);
+        $entityCode = (string)$input->getArgument(self::ARG_ENTITY_TYPE);
+        $entityId   = (string)$input->getArgument(self::ARG_ENTITY_ID);
+
+        $entities = explode(',', $entityId);
+
+        $project = $this->projectRepository->getById($projectId);
+
+        try {
+            $entitySeeder = $this->entitySeederPool->getByCode($entityCode);
+        } catch (\Exception $e) {
+            $output->writeln(sprintf('<error>%s: seeder not found</error>', $entityCode));
+
+            return;
+        }
+
+        $result = $entitySeeder->seed($project, $entities);
+
+        if ($result === true) {
+            $output->writeln(sprintf('<info>%s: seeding successful</info>', $entityCode));
+        } else {
+            $output->writeln(sprintf('<error>%s: seeding not successful</error>', $entityCode));
+        }
+
+    }
+} 
