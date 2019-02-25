ab8452a34aa29f5354e92f82eef9192dc203341e
Matthias Walter <matthias.walter@mwltr.de>
2019-02-25 15:34:54 +0100

    ETM2-38 StoreColumn in Project Grid

---
+++ Ui/Component/Listing/Columns/StoreColumn.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+/**
+ * @copyright see PROJECT_LICENSE.txt
+ *
+ * @see PROJECT_LICENSE.txt
+ */
+
+namespace Eurotext\TranslationManager\Ui\Component\Listing\Columns;
+
+use Magento\Framework\Escaper;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Store\Model\System\Store as SystemStore;
+use Magento\Store\Ui\Component\Listing\Column\Store;
+
+class StoreColumn extends Store
+{
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        SystemStore $systemStore,
+        Escaper $escaper,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $systemStore, $escaper, $components, $data, $data['name']);
+    }
+
+}
